2cbe2617133760b976bf1532c141512d8b08be03
Eric Vandenberg <Eric1827v@gmail.com>
2015-03-21 21:29:19 -0700

    Bootstrap Player Styling

--- includes/header.php
+++ includes/header.php
@@ -1,63 +1,53 @@
-    <header id="main_header">
-        <div class="row">
-            <div class=".col-md-6 .col-md-offset-3">
-                <div id="todo-add" class="panel panel-default">
-                    <div class="panel-heading">Upload Your Music</div>
-                    <div class="panel-body">
-                        <div class="container">
-                            <div class="header">
-                                <noindex>
-                                <ul class="nav nav-pills pull-right">
-                                    <li><?php ?></li>
-                                </ul>
-                                </noindex>
+<header id="main_header">
+    <div class="container">
+        <div class="header">
+            <noindex>
+            <ul class="nav nav-pills pull-right">
+                <li><?php ?></li>
+            </ul>
+            </noindex>
 
-                                <h1 itemprop="name">MUSIC | BLOG</h1>
-                            </div>
+            <h1 itemprop="name">MUSIC | BLOG</h1>
+        </div>
 
-                            <div id="demo">
-                                <div id="waveform">
-                                    <div class="progress progress-striped active" id="progress-bar">
-                                        <div class="progress-bar progress-bar-info"></div>
-                                    </div>
-
-                                    <!-- Here be the waveform -->
-                                </div>
-
-                                <div class="controls">
-                                    <button class="btn btn-primary" data-action="back">
-                                        <i class="glyphicon glyphicon-step-backward"></i>
-                                        Backward
-                                    </button>
-
-                                    <button class="btn btn-primary" data-action="play">
-                                        <i class="glyphicon glyphicon-play"></i>
-                                        Play
-                                        /
-                                        <i class="glyphicon glyphicon-pause"></i>
-                                        Pause
-                                    </button>
-
-                                    <button class="btn btn-primary" data-action="forth">
-                                        <i class="glyphicon glyphicon-step-forward"></i>
-                                        Forward
-                                    </button>
-
-                                    <button class="btn btn-primary" data-action="toggle-mute">
-                                        <i class="glyphicon glyphicon-volume-off"></i>
-                                        Toggle Mute
-                                    </button>
-                                    <form action="../public/actions/uploadhandler.php" method="post" enctype="multipart/form-data">
-                                        Select image to upload:
-                                        <button class="btn btn-primary"><input type="file" name="fileToUpload" id="fileToUpload"></button>
-                                        <button class="btn btn-primary"><input type="submit" value="Upload File" name="submit"></button>
-                                    </form>
-                                </div>
-                            </div>
-                    </div>
-                    </div>
+        <div id="demo">
+            <div id="waveform">
+                <div class="progress progress-striped active" id="progress-bar">
+                    <div class="progress-bar progress-bar-info"></div>
                 </div>
+
+                <!-- Here be the waveform -->
+            </div>
+
+            <div class="controls">
+                <button class="btn btn-info btn-md active" data-action="back">
+                    <i class="glyphicon glyphicon-step-backward"></i>
+                    Backward
+                </button>
+
+                <button class="btn btn-info btn-md active" data-action="play">
+                    <i class="glyphicon glyphicon-play"></i>
+                    Play
+                    /
+                    <i class="glyphicon glyphicon-pause"></i>
+                    Pause
+                </button>
+
+                <button class="btn btn-info btn-md active" data-action="forth">
+                    <i class="glyphicon glyphicon-step-forward"></i>
+                    Forward
+                </button>
+
+                <button class="btn btn-info btn-md active" data-action="toggle-mute">
+                    <i class="glyphicon glyphicon-volume-off"></i>
+                    Toggle Mute
+                </button>
+                <form action="../public/actions/uploadhandler.php" method="post" enctype="multipart/form-data">
+                    Select mp3 or mp4 file to upload:
+                    <input class="btn btn-info btn-md active" type="file" name="fileToUpload" id="fileToUpload">
+                    <input class="btn btn-info btn-md active" type="submit" value="Upload File" name="submit">
+                </form>
             </div>
         </div>
-    </header>
+</header>
 
