af2569846458406f38a16b358d411893f8b8b7ba
Eric Vandenberg <Eric1827v@gmail.com>
2015-03-23 02:53:39 -0700

    New post inserted into MySQL

--- public/actions/add.php
+++ public/actions/add.php
@@ -1,8 +1,8 @@
-<pre><?php
+<?php
 require_once('connect.php');
-// $time = strtotime($_POST['date']);
-// $query = "INSERT INTO  (title, timestamp, details) VALUES ('$_POST[title]', $time, '$_POST[details]')";
-// $todos = mysqli_query($con, $query);
-print_r($_POST);
 
- ?></pre>
+$query = "INSERT INTO `blog_post` (`UID`, `file`, `song`, `artist`, `key`, `bpm`, `info`) VALUES ('{$_POST[UID]}', '{$_POST[file]}', '{$_POST[song]}', '{$_POST[artist]}', '{$_POST[key]}', '{$_POST[bpm]}', '{$_POST[info]}')";
+$posts = mysqli_query($con, $query);
+
+
+ ?>
--- public/actions/uploadhandler.php
+++ public/actions/uploadhandler.php
@@ -1,10 +1,10 @@
 <pre><?php
 
 //make a variable, $target_dir, which indicates where files will be uploaded.  In this case, the uploads directory.  Don't forget the separating / between the directory and any file that will come after.
-$target_dir = "../../includes/uploads/";
+$target_dir = "../includes/uploads/";
 //make a variable, $target_file, that combines the target directory with the uploaded file name.  This can be found in the $_FILES superglobal, in the fileToUpload key, in the name subkey
 $target_file = $target_dir . $_FILES['fileToUpload']['name'];
-
+print_r($_FILES);
 
 //set an initial flag to indicate success.  $uploadOK.  Set it initially to true
 $uploadOK = true;
@@ -54,8 +54,7 @@ if ($uploadOK)
         //echo a success message for the upload
         echo "Successful upload";
         //echo an HTML img tag with the $target_file as its src
-
-        //echo "<img src='$target_file'>";
+        echo "<img src='$target_file'>";
     }
     //else if $move_succeeded wasn't true
     else 
@@ -67,6 +66,6 @@ if ($uploadOK)
     //else if the $uploadOk wasn't true
 
 //echo a failure message, because some criteria didn't match
-    echo " Do the Happy Dance";
+    echo "Something went wrong";
 }
 ?></pre>
